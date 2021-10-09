40d73da234560dbe68824ec9d383a668f6819a66
Emmanuel Awotunde <awotunde.emmanuel1@gmail.com>
2021-10-09 17:49:11 +0100

    chore: add uuid validation to user domain

--- packages/domains/user/routes/v1-get-user.ts
+++ packages/domains/user/routes/v1-get-user.ts
@@ -1,13 +1,20 @@
-import { ApiError } from '@nc/utils/errors';
 import { getUserDetails } from '../model';
 import { Router } from 'express';
 import { secureTrim } from '../formatter';
 import { to } from '@nc/utils/async';
+import validate from 'uuid-validate';
+import { ApiError, BadRequest } from '@nc/utils/errors';
 
 export const router = Router();
 
 router.get('/get-user-details', async (req, res, next) => {
-  const [userError, userDetails] = await to(getUserDetails(req.query?.userId));
+  const userId = req.query?.userId;
+
+  if (!userId || !validate(userId)) {
+    return next(BadRequest('Please input a valid uuid', req));
+  }
+
+  const [userError, userDetails] = await to(getUserDetails(userId));
 
   if (userError) {
     return next(new ApiError(userError, userError.status, `Could not get user details: ${userError}`, userError.title, req));
@@ -17,5 +24,5 @@ router.get('/get-user-details', async (req, res, next) => {
     return res.json({});
   }
 
-  return res.json(secureTrim(userDetails));
+  return res.json(JSON.parse(secureTrim(userDetails)));
 });
